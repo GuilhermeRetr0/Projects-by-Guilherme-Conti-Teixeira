d78138a734f3fe29f4163cd92abb607e9baafe5f
Guilherme Conti Teixeira <68434142+GuilhermeRetr0@users.noreply.github.com>
2023-04-17 11:27:03 -0300

    Add files via upload
    Uma simples lista de compras para fácil entendimento da lógica de programação

---
+++ Lista_de_compras.js
@@ -0,0 +1,8 @@
+let listaDeCompras = [];
+
+listaDeCompras.push("Arroz");
+listaDeCompras.push("Feijão");
+listaDeCompras.push("Macarrão");
+listaDeCompras.push("Carne");
+
+console.log(listaDeCompras);
